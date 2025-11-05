b130562341d6ecef32439a1feacbc0c986efd46e
Gionnio <Gionnio@users.noreply.github.com>
2025-11-05 18:20:58 +0100

    Initial Commit

---
+++ media renamer2.0/media_renamer2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  media_renamer2_0App.swift
+//  media renamer2.0
+//
+//  Created by Gianluca Checcarini on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct media_renamer2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
